2d7a64e45f6a035f337f8c1ae78c5c29456fb7fa
K Sri Sai Nitin <113088524+Tintin07k@users.noreply.github.com>
2023-07-18 02:22:22 +0530

    Delete EVoting directory

--- EVoting/JavaDoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.amrita.jpl.cys21027.project","c":"EVotingApplication","l":"EVotingApplication()","u":"%3Cinit%3E()"},{"p":"com.amrita.jpl.cys21027.project","c":"EVotingApplication","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
